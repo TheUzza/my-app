01b22bc89b15cde3121b9d893d1451344687b408
Uzza <salamsyr12213@gmail.com>
2023-01-03 16:47:31 +0100

    This is simply broken

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import {Home} from "./Pages/Home"
+import {Person} from "./pages/Person"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function app(){
+  retrun (
+    <Routes>
+      <Route path="/" element={ <home /> } />
+      <Route path="/Persons" element={<Person/>} />
+    </Routes>
+  )
 }
-
-export default App;
+  
+export default app
